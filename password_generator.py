c32d9dc29321eae071e545e1672be983642b52fe
aryanshu1911 <87239827+aryanshu1911@users.noreply.github.com>
2025-02-08 02:15:36 +0530

    password_generator.py

---
+++ password_generator.py
@@ -0,0 +1,25 @@
+# Program to create a Password Generator...
+
+# importing string and random libraries
+import string
+import random
+
+if __name__ == '__main__':
+    upper = string.ascii_uppercase
+    lower = string.ascii_lowercase
+    digit = string.digits
+    special = string.punctuation
+
+    pass_len = int(input("Enter the length of the password: "))
+
+    # Create an empty list where all characters are to be stored
+    s = []
+    s.extend(upper)
+    s.extend(lower)
+    s.extend(digit)
+    s.extend(special)
+
+    # Shuffle the characters in the list s to randomize the password order
+    random.shuffle(s)   
+    password = ''.join(s[0:pass_len])
+    print("Your password is: ", password)
